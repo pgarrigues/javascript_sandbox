36dc5662aac837b931dab7da2e715d34b519a9ba
Pierre-Loup Garrigues <pierre-loupg@hotmail.fr>
2021-02-16 23:03:16 +0100

    bouton gradient

---
+++ bouton_gradient/script.js
@@ -0,0 +1,11 @@
+
+document.getElementById('button').addEventListener('mousemove', (e) => {
+    const x = e.pageX - e.target.offsetLeft;
+    const y = e.pageY - e.target.offsetTop;
+
+    console.log(`${x} + ${y}`);
+
+    e.target.style.setProperty('--x', `${x}px`);
+    e.target.style.setProperty('--y', `${y}px`);
+
+});
